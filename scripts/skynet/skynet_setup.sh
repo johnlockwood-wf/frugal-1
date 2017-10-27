afbe7dd43d85cda4f9b5fbb8127e5af344027aaf
John.Lockwood <john.lockwood@workiva.com>
2017-10-26 22:50:46 -0700

    Eliminate godep from skynet

--- scripts/skynet/skynet_setup.sh
+++ scripts/skynet/skynet_setup.sh
@@ -4,11 +4,6 @@ set -exo pipefail
 
 mkdir -p /go/src/github.com/Workiva/
 
-# Move godeps to gopath for both library and binary
-cp -r /testing/Godeps/_workspace/* $GOPATH/
-cp -r /testing/lib/go/Godeps/_workspace/* $GOPATH/
-
-
 # Symlink frugal to gopath - this allows skynet-cli editing for interactive/directmount
 ln -s /testing/ /go/src/github.com/Workiva/frugal
 
@@ -20,7 +15,7 @@ if [ -z "${IN_SKYNET_CLI+yes}" ]; then
     chmod 755 ${GOPATH}/bin/frugal
 else
     cd $GOPATH/src/github.com/Workiva/frugal
-    godep go install
+    go install
 fi
 
 
