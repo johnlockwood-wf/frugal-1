f3b8c164add0b48912e7437aab2a94e3232e3ff8
John.Lockwood <john.lockwood@workiva.com>
2017-10-26 23:02:54 -0700

    Add real remove godep change instead of the wrong one

--- scripts/skynet/cross/setup_go.sh
+++ scripts/skynet/cross/setup_go.sh
@@ -8,5 +8,5 @@ cd ${FRUGAL_HOME}
 
 # Create Go binaries
 rm -rf test/integration/go/bin/*
-godep go build -o test/integration/go/bin/testclient test/integration/go/src/bin/testclient/main.go
-godep go build -o test/integration/go/bin/testserver test/integration/go/src/bin/testserver/main.go
+go build -o test/integration/go/bin/testclient test/integration/go/src/bin/testclient/main.go
+go build -o test/integration/go/bin/testserver test/integration/go/src/bin/testserver/main.go
