c09020358d96790b14f54e601cf80e1e6c79d361
John.Lockwood <john.lockwood@workiva.com>
2017-10-31 15:15:26 -0700

    Install own version of frugal

--- scripts/skynet/skynet_setup.sh
+++ scripts/skynet/skynet_setup.sh
@@ -8,15 +8,8 @@ mkdir -p /go/src/github.com/Workiva/
 ln -s /testing/ /go/src/github.com/Workiva/frugal
 
 # Install frugal
-if [ -z "${IN_SKYNET_CLI+yes}" ]; then
-    mkdir -p $GOPATH/bin
-    cp ${SKYNET_APPLICATION_FRUGAL_RELEASE} $GOPATH/bin/frugal
-    mkdir $GOPATH/src/github.com/Workiva/frugal/test/integration/log
-    chmod 755 ${GOPATH}/bin/frugal
-else
-    cd $GOPATH/src/github.com/Workiva/frugal
-    go install
-fi
+cd $GOPATH/src/github.com/Workiva/frugal
+go install
 
 
 # Start gnatsd
